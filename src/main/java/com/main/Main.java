5c6bf26593ae92ca165b661d95e4fad0c6abecac
eunhye park <lime1pm@naver.com>
2022-08-23 20:07:35 +0900

    Fix Main class
    모듈 적용

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,9 +1,12 @@
 package com.main;
 
+import com.api.TestApi;
+import com.request.HttpRequestListener;
+import com.response.HttpResponseSender;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,57 +20,45 @@ public class Main {
         while(true) {
             Socket socket = serverSocket.accept();
 
-            InputStream is = socket.getInputStream();
-            BufferedInputStream bis = new BufferedInputStream(is,8192);
-            InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);
-            BufferedReader br = new BufferedReader(isr,8192);
+            HttpRequestListener httpRequestListener = HttpRequestListener.of(socket.getInputStream());
+            String requestInput = httpRequestListener.listen();
+            System.out.println(requestInput);
 
-            String line = "";
-            StringBuilder input = new StringBuilder();
+            StringTokenizer stringTokenizer = new StringTokenizer(requestInput,"\n");
 
-            while((line=br.readLine()) != null && !Objects.equals(line,"")) {
-                input.append(line).append("\n");
+            if (stringTokenizer.countTokens() < 1){
+                continue;
             }
-            System.out.println(input.toString());
-
-            StringTokenizer stringTokenizer = new StringTokenizer(input.toString(),"\n");
 
             String startLine = stringTokenizer.nextToken();
             String api = startLine.split(" ")[1];
 
-            OutputStream os = socket.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os,8192);
-            OutputStreamWriter osw = new OutputStreamWriter(bos,StandardCharsets.UTF_8);
+            StringBuilder responseMessage = new StringBuilder();
 
-            osw.write("HTTP/1.1 200 OK\n"+
+            responseMessage.append("HTTP/1.1 200 OK\n"+
                     "Content-Type: text/html;charset=UTF-8\n"+
                     "\n");
 
             if(Objects.equals("/",api)){
-                osw.write(readFile(Paths.get("src","main","resources","test.html"))+"\n");
+                responseMessage.append(readFile(Paths.get("src","main","resources","test.html"))).append("\n");
             }else {
                 StringTokenizer apiTokenizer = new StringTokenizer(api.substring(1),"\\?");
                 String path = apiTokenizer.nextToken();
 
                 if (Objects.equals(path,"test")){
+                    TestApi testApi = new TestApi();
                     String values = apiTokenizer.nextToken();
-                    String name = "";
-                    StringTokenizer valueTokenizer = new StringTokenizer(values,"=");
+                    String testResponse = testApi.getResponseMessage(values);
 
-                    if (Objects.equals("name",valueTokenizer.nextToken())){
-                        name = URLDecoder.decode(valueTokenizer.nextToken(),StandardCharsets.UTF_8);
-                    }
-
-                    osw.write("안녕하세요 "+name+"님\n");
+                    responseMessage.append(testResponse).append("\n");
                 }else {
-                    osw.write(readFile(Paths.get("src","main","resources","error.html"))+"\n");
+                    responseMessage.append(readFile(Paths.get("src","main","resources","error.html"))).append("\n");
                 }
             }
 
-            osw.write("\n");
-
-            osw.flush();
-            osw.close();
+            HttpResponseSender httpResponseSender = HttpResponseSender.of(socket.getOutputStream());
+            httpResponseSender.send(responseMessage.toString());
+            httpResponseSender.close();
         }
     }
 
