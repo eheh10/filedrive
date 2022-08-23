6c300deb65954a450d4079a16b3cb1215633e5f2
eunhye park <lime1pm@naver.com>
2022-08-23 23:41:18 +0900

    Fix TestApi class
    null 체크, 예외 처리 추가

--- src/main/java/com/api/TestApi.java
+++ src/main/java/com/api/TestApi.java
@@ -8,20 +8,29 @@ import java.util.StringTokenizer;
 public class TestApi {
 
     public String getResponseMessage(String values){
-        StringBuilder response = new StringBuilder();
-
-        String name = "";
-        StringTokenizer valueTokenizer = new StringTokenizer(values,"=");
-
-        if (Objects.equals("name",valueTokenizer.nextToken())){
-            name = URLDecoder.decode(valueTokenizer.nextToken(),StandardCharsets.UTF_8);
+        if (values == null){
+            throw new RuntimeException("values가 null");
         }
 
-        response.append("안녕하세요 ")
-                .append(name)
-                .append("님");
+        StringBuilder response = new StringBuilder();
+        StringTokenizer valueTokenizer = new StringTokenizer(values,"&");
 
-        return response.toString();
+        while (valueTokenizer.hasMoreTokens()){
+            StringTokenizer tokenizer = new StringTokenizer(valueTokenizer.nextToken(),"=");
+            String key = tokenizer.nextToken();
+
+            if (Objects.equals("name",key)){
+                String name = URLDecoder.decode(tokenizer.nextToken(), StandardCharsets.UTF_8);
+
+                response.append("안녕하세요 ")
+                        .append(name)
+                        .append("님");
+
+                return response.toString();
+            }
+        }
+
+        throw new RuntimeException("name key가 없음");
     }
 
 }
