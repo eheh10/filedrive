27826621c17ed3806dfddc949a14e64c2e5102ec
eunhye park <lime1pm@naver.com>
2022-08-24 12:53:21 +0900

    FIX TestApiTest

--- src/test/java/com/api/TestApiTest.java
+++ src/test/java/com/api/TestApiTest.java
@@ -1,7 +1,6 @@
 package com.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.parser.KeyParser;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,25 +14,26 @@ class TestApiTest {
         String expected = "안녕하세요 kim님";
         TestApi testApi = new TestApi();
         String values = "name=kim";
+        KeyParser nameKeyParser = KeyParser.of(values);
 
         //when
-        String actual = testApi.getResponseMessage(values);
+        String actual = testApi.getResponseMessage(nameKeyParser);
 
         //then
         Assertions.assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @DisplayName("value가 null일때 런타임에러 발생테스트")
+    @DisplayName("KeyParser가 null일때 런타임에러 발생테스트")
     void testWithNull() {
         //given
         TestApi testApi = new TestApi();
-        String expected = "values가 null";
-        String values = null;
+        String expected = "KeyParser가 null";
+        KeyParser nameKeyParser = null;
 
         try{
             //when
-            String result = testApi.getResponseMessage(values);
+            String result = testApi.getResponseMessage(nameKeyParser);
         }catch (RuntimeException e){
 
             //then
@@ -41,21 +41,5 @@ class TestApiTest {
         }
     }
 
-    @Test
-    @DisplayName("value에 name key가 없을때 런타임에러 발생테스트")
-    void testWithNoName() {
-        //given
-        TestApi testApi = new TestApi();
-        String expected = "name key가 없음";
-        String values = "";
-
-        try{
-            //when
-            String result = testApi.getResponseMessage(values);
-        }catch (RuntimeException e){
-            //then
-            Assertions.assertThat(e.getMessage()).isEqualTo(expected);
-        }
-    }
 
 }
