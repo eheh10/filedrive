bf01d515253ae3953a5b0196b6b5d10f4c2d4fd1
eunhye park <lime1pm@naver.com>
2022-08-25 02:42:43 +0900

    REFACTOR ResponseMessageCreator

--- src/main/java/com/main/ResponseMessageCreator.java
+++ src/main/java/com/main/ResponseMessageCreator.java
@@ -1,13 +1,9 @@
 package com.main;
 
-import com.api.TestApi;
-import com.parser.KeyParser;
-import com.request.InputStreamListener;
+import com.api.ApiFactory;
+import com.api.TargetApi;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.StringTokenizer;
 
@@ -18,9 +14,6 @@ public class ResponseMessageCreator {
             throw new RuntimeException("requestInput이 null");
         }
 
-        String requestInput = inputstreamListener.listen();
-        System.out.println(requestInput);
-
         if (requestInput.length() == 0) {
             return "";
         }
@@ -29,56 +22,41 @@ public class ResponseMessageCreator {
         StringTokenizer tokenizer = new StringTokenizer(startLine," ");
         String method = tokenizer.nextToken();
         String target = tokenizer.nextToken();
+        String path = target.split("\\?")[0];
+
+        String values = parseValues(method,target,requestInput);
+
+        TargetApi targetApi = ApiFactory.of(path,values);
+        String body = targetApi.getBody();
 
         StringBuilder responseMessage = new StringBuilder();
 
         responseMessage.append("HTTP/1.1 200 OK\n"+
                 "Content-Type: text/html;charset=UTF-8\n"+
-                "\n");
-
-        String path = target.split("\\?")[0];
-        if(Objects.equals("/",path)){
-            responseMessage.append(readFile(Paths.get("src","main","resources","test.html"))).append("\n");
-            return responseMessage.toString();
-        }
-
-        if (Objects.equals(path,"/test")){
-            TestApi testApi = new TestApi();
-            String values = "";
-
-            if (Objects.equals(method,"GET")) {
-                values = target.split("\\?")[1];
-            }else if (Objects.equals(method,"POST")) {
-                values = requestInput.split("\n\n")[1].strip();
-            }
-
-            KeyParser nameKeyParser = KeyParser.of(values);
-            String testResponse = testApi.getResponseMessage(nameKeyParser);
-
-            responseMessage.append(testResponse).append("\n");
-        }else {
-            responseMessage.append(readFile(Paths.get("src","main","resources","error.html"))).append("\n");
-        }
+                "\n")
+                .append(body)
+                .append("\n");
 
         return responseMessage.toString();
     }
 
-    private String readFile(Path path) throws IOException {
-        if (path==null) {
-            throw new RuntimeException("존재하지 않는 경로");
+    private String parseValues(String method, String target, String requestInput) {
+        String values = "";
+
+        if (Objects.equals(method,"GET")) {
+            String[] elements = target.split("\\?");
+
+            if(elements.length < 2){
+                return "";
+            }
+
+            return elements[1];
         }
 
-        InputStream is = new FileInputStream(path.toString());
-        BufferedInputStream bis = new BufferedInputStream(is,8192);
-        InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);
-
-        int len = 0;
-        char[] buffer = new char[100];
-        StringBuilder output = new StringBuilder();
-        while((len=isr.read(buffer))!=-1){
-            output.append(buffer,0,len);
+        if (Objects.equals(method,"POST")) {
+            return requestInput.split("\n\n")[1].strip();
         }
 
-        return output.toString();
+        return values;
     }
 }
