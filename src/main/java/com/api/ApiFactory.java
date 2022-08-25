a956b0c3df92c718c7566668407d24fd9ca52414
eunhye park <lime1pm@naver.com>
2022-08-26 00:47:52 +0900

    FIX ApiFactory
    변경된 TestApi 구조 반영

--- src/main/java/com/api/ApiFactory.java
+++ src/main/java/com/api/ApiFactory.java
@@ -1,17 +1,20 @@
 package com.api;
 
+import com.parser.KeyParser;
+
 import java.util.Objects;
 
 public class ApiFactory {
     public static TargetApi of(String path, String values) {
         if(Objects.equals("/",path)){
-            return new NameApi();
+            return new FileApi("test.html");
         }
 
         if (Objects.equals(path,"/test")){
-            return TestApi.of(values);
+            String nameValue = KeyParser.of(values).getValue("name");
+            return new TestApi(nameValue);
         }
 
-        return new ErrorApi();
+        return new FileApi("error.html");
     }
 }
