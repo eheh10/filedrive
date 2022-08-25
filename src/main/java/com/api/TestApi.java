be2661921a75d0db176ce473fc7704094da08f8f
eunhye park <lime1pm@naver.com>
2022-08-26 00:45:15 +0900

    FIX KeyParser -> String in TestApi class

--- src/main/java/com/api/TestApi.java
+++ src/main/java/com/api/TestApi.java
@@ -1,26 +1,18 @@
 package com.api;
 
-import com.parser.KeyParser;
-
 public class TestApi implements TargetApi{
-    private final KeyParser keyParser;
+    private final String name;
 
-    public TestApi(KeyParser keyParser) {
-        if (keyParser==null) {
-            throw new RuntimeException("KeyParser가 null");
+    public TestApi(String name) {
+        if (name==null) {
+            throw new RuntimeException("name이 null");
         }
-        this.keyParser = keyParser;
+        this.name = name;
     }
 
-    public static TestApi of(String values) {
-        KeyParser keyParser = KeyParser.of(values);
-        return new TestApi(keyParser);
-    }
 
     @Override
     public String getBody() {
-        String name = keyParser.getValue("name");
-
         return "안녕하세요 "+name+"님";
     }
 }
