397b59a3b91844c2c24be25a1ce4caa4e6375577
eunhye park <lime1pm@naver.com>
2022-09-03 00:35:39 +0900

    FIX StatLine
    Add valid check in parse()
    Fix Enum Method

--- src/main/java/com/request/StartLine.java
+++ src/main/java/com/request/StartLine.java
@@ -2,18 +2,31 @@ package com.request;
 
 import com.method.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class StartLine {
 
-    private enum Method {
+    public enum Method {
         GET(new GetMethod()),
         POST(new PostMethod()),
         PUT(new PutMethod()),
         DELETE(new DeleteMethod());
 
         private final HttpMethod httpMethod;
+        private static final Set<String> methods = createValues();
+
+        private static Set<String> createValues() {
+            Set<String> methods = new HashSet<>();
+
+            for(Method h:values()) {
+                methods.add(h.name());
+            }
+
+            return methods;
+        }
 
         Method(HttpMethod httpMethod) {
             this.httpMethod = httpMethod;
@@ -21,6 +34,10 @@ public class StartLine {
 
         public HttpMethod getHttpMethod() {
             return httpMethod;
+        }
+
+        public static boolean contains(String method) {
+            return methods.contains(method);
         }
 
     }
@@ -47,17 +64,37 @@ public class StartLine {
     }
 
     public static StartLine parse(String startLine) {
+        if (startLine == null || startLine.isBlank()) {
+            throw new RuntimeException();
+        }
+
         StringTokenizer tokenizer = new StringTokenizer(startLine," ");
+        if (tokenizer.countTokens() != 3) {
+            throw new RuntimeException();
+        }
 
         String method = tokenizer.nextToken();
         Method httpMethod = Method.valueOf(method);
 
+        if (!Method.contains(method)) {
+            throw new RuntimeException();
+        }
+
         String path = tokenizer.nextToken();
+
+        if(!Objects.equals(path.charAt(0),'/')) {
+            throw new RuntimeException();
+        }
+
         if (Objects.equals(method,"GET")) {
             path = path.split("\\?")[0];
         }
 
         String version = tokenizer.nextToken();
+
+        if(version.length() < 6 || !Objects.equals(version.substring(0,5),"HTTP/")) {
+            throw new RuntimeException();
+        }
 
         return new StartLine(httpMethod,path,version);
     }
