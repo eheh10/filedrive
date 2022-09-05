0bacef153d8e282283e88816ee12b4c96a386228
eunhye park <lime1pm@naver.com>
2022-09-05 23:50:57 +0900

    FIX HeaderTest

--- src/test/java/com/request/HeaderTest.java
+++ src/test/java/com/request/HeaderTest.java
@@ -1,21 +1,47 @@
 package com.request;
 
-import com.exception.InvalidValueException;
+import com.exception.NotPositiveNumberException;
 import com.exception.NullException;
 import com.exception.StatusCode431Exception;
+import com.field.Field;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class HeaderTest {
+    private int limitLength = 8192;
     private String fields = "Host: localhost\n" +
             "Connection: keep-alive\n" +
             "Content-Length: 116\n\n";
+
+    private Header revertHeader(String fields) {
+        Map<String,Field> values = new HashMap<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(fields,"\n");
+        while(tokenizer.hasMoreTokens()) {
+
+            StringTokenizer filedTokenizer = new StringTokenizer(tokenizer.nextToken(),":");
+            String filedName = filedTokenizer.nextToken().strip().toUpperCase();
+            String filedValues = filedTokenizer.nextToken().strip();
+
+            StringTokenizer valueTokenizer = new StringTokenizer(filedValues, ",");
+            List<String> fieldValues = new ArrayList<>(Math.max(10, valueTokenizer.countTokens()));
+
+            while (valueTokenizer.hasMoreTokens()) {
+                fieldValues.add(valueTokenizer.nextToken().strip());
+            }
+
+            Field field = new Field(filedName, fieldValues);
+
+            values.put(filedName, field);
+        }
+
+        return new Header(Collections.unmodifiableMap(values));
+    }
 
     private BufferedReader getBufferedReader(String str) {
         InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
@@ -26,16 +52,26 @@ class HeaderTest {
     }
 
     @Test
-    @DisplayName("value가 여러개인 필드 정상 파싱 테스트")
-    void testParsingHeaderWithNormalCase() throws IOException {
+    @DisplayName("value가 한개인 필드 정상 파싱 테스트")
+    void testParsingHeaderWithNormalCase1() throws IOException {
         //given
-        Map<String,List<String>> expected = Map.of(
-                "HOST",List.of("localhost"),
-                "CONNECTION",List.of("keep-alive"),
-                "CONTENT-LENGTH",List.of("116")
-        );
+        String field = "Host: localhost\n\n";
+        Header expected = revertHeader(field);
+        BufferedReader br = getBufferedReader(field);
+
+        //when
+        Header actual = Header.parse(br,limitLength);
+
+        //then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("value가 여러개인 필드 정상 파싱 테스트")
+    void testParsingHeaderWithNormalCase2() throws IOException {
+        //given
+        Header expected = revertHeader(fields);
         BufferedReader br = getBufferedReader(fields);
-        int limitLength = 8192;
 
         //when
         Header actual = Header.parse(br,limitLength);
@@ -48,12 +84,9 @@ class HeaderTest {
     @DisplayName("field value 가 여러개일때 정상 파싱 테스트")
     void testParsingHeaderWithValues() throws IOException {
         //given
-        Map<String,List<String>> expected = Map.of(
-                "ACCEPT-LANGUAGE",List.of("ko-KR","ko;q=0.9","en-US;q=0.8","en;q=0.7")
-        );
         String field = "Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7\n\n";
+        Header expected = revertHeader(field);
         BufferedReader br = getBufferedReader(field);
-        int limitLength = 8192;
 
         //when
         Header actual = Header.parse(br,limitLength);
@@ -63,66 +96,10 @@ class HeaderTest {
     }
 
     @Test
-    @DisplayName("BufferedReader 가 null 일때 런타임에러 발생 테스트")
-    void testParsingWithNull() throws IOException {
-        //given
-        String msg = "HeaderParser.parse().BufferedReader is null";
-        BufferedReader br = null;
-        int limitLength = 8192;
-
-        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
-                .isInstanceOf(NullException.class)
-                .hasMessageContaining(msg);
-    }
-
-    @Test
-    @DisplayName("limitLength 가 0일때 런타임에러 발생 테스트")
-    void testParsingWithZeroLimitLength() throws IOException {
-        //given
-        String msg = "HeaderParser.parse().limitLength must be greater than 0";
-        BufferedReader br = getBufferedReader(fields);
-        int limitLength = 0;
-
-        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
-                .isInstanceOf(InvalidValueException.class)
-                .hasMessageContaining(msg);
-    }
-
-    @Test
-    @DisplayName("limitLength 가 0이하일때 런타임에러 발생 테스트")
-    void testParsingWithWrongLimitLength() throws IOException {
-        //given
-        String msg = "HeaderParser.parse().limitLength must be greater than 0";
-        BufferedReader br = getBufferedReader(fields);
-        int limitLength = -1;
-
-        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
-                .isInstanceOf(InvalidValueException.class)
-                .hasMessageContaining(msg);
-    }
-
-    @Test
-    @DisplayName("limitLength 를 초과했을때 런타임에러 발생 테스트")
-    void testParsingWithExceedingLimitLength() throws IOException {
-        //given
-        String msg = "431 Request header too large";
-        BufferedReader br = getBufferedReader(fields);
-        int limitLength = 10;
-
-        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
-                .isInstanceOf(StatusCode431Exception.class)
-                .hasMessageContaining(msg);
-    }
-
-    @Test
     @DisplayName("default limitLength 정상 파싱 테스트")
     void testParsingWithDefaultLimitLength() throws IOException {
         //given
-        Map<String,List<String>> expected = Map.of(
-                "HOST",List.of("localhost"),
-                "CONNECTION",List.of("keep-alive"),
-                "CONTENT-LENGTH",List.of("116")
-        );
+        Header expected = revertHeader(fields);
         BufferedReader br = getBufferedReader(fields);
 
         //when
@@ -131,4 +108,48 @@ class HeaderTest {
         //then
         Assertions.assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("BufferedReader 가 null 일때 런타임에러 발생 테스트")
+    void testParsingWithNull() throws IOException {
+        //given
+        BufferedReader br = null;
+
+        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
+                .isInstanceOf(NullException.class);
+    }
+
+    @Test
+    @DisplayName("limitLength 가 0일때 런타임에러 발생 테스트")
+    void testParsingWithZeroLimitLength() throws IOException {
+        //given
+        BufferedReader br = getBufferedReader(fields);
+        limitLength = 0;
+
+        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
+                .isInstanceOf(NotPositiveNumberException.class);
+    }
+
+    @Test
+    @DisplayName("limitLength 가 0이하일때 런타임에러 발생 테스트")
+    void testParsingWithWrongLimitLength() throws IOException {
+        //given
+        BufferedReader br = getBufferedReader(fields);
+        limitLength = -1;
+
+        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
+                .isInstanceOf(NotPositiveNumberException.class);
+    }
+
+    @Test
+    @DisplayName("limitLength 를 초과했을때 런타임에러 발생 테스트")
+    void testParsingWithExceedingLimitLength() throws IOException {
+        //given
+        BufferedReader br = getBufferedReader(fields);
+        limitLength = 10;
+
+        Assertions.assertThatThrownBy(()->Header.parse(br,limitLength))
+                .isInstanceOf(StatusCode431Exception.class);
+    }
+
 }
