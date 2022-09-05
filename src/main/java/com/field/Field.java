12ed8cdcb4c63c4e39ba6b8672695f2114a78961
eunhye park <lime1pm@naver.com>
2022-09-05 23:52:33 +0900

    FIX Field

--- src/main/java/com/field/Field.java
+++ src/main/java/com/field/Field.java
@@ -1,6 +1,7 @@
 package com.field;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Field {
     private final String name;
@@ -18,5 +19,23 @@ public class Field {
                 "name='" + name + '\'' +
                 ", values=" + values +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Field field = (Field) o;
+
+        if (!Objects.equals(name, field.name)) return false;
+        return Objects.equals(values, field.values);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (values != null ? values.hashCode() : 0);
+        return result;
     }
 }
