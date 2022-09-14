c62394fa173d1ec52923b9b929302cbb2ffcee0e
eunhye park <lime1pm@naver.com>
2022-09-14 23:19:48 +0900

    FIX HttpRequestHandlers

--- src/main/java/com/api/HttpRequestHandlers.java
+++ src/main/java/com/api/HttpRequestHandlers.java
@@ -6,30 +6,35 @@ import com.exception.NullException;
 import com.method.HttpRequestMethod;
 import com.path.HttpRequestPath;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequestHandlers {
-    private static final Map<HttpRequestPath, Map<HttpRequestMethod, HttpRequestHandler>> VALUES = Map.of(
-            HttpRequestPath.of("/test"), Map.of(
-                    HttpRequestMethod.POST,new HttpRequestBodyFileCreator()
-            )
-    );
+    private static Map<HttpRequestPath, Map<HttpRequestMethod, HttpRequestHandler>> values = new HashMap<>();
+
+    public void register(HttpRequestPath path, HttpRequestMethod method, HttpRequestHandler handler) {
+        if (path == null || method == null || handler == null) {
+            throw new NullException();
+        }
+
+        values.put(path,Map.of(method,handler));
+    }
 
     public HttpRequestHandler find(HttpRequestPath path, HttpRequestMethod method) {
         if (path == null || method == null) {
             throw new NullException();
         }
 
-        if (!VALUES.containsKey(path)) {
+        if (!values.containsKey(path)) {
             throw new NotFoundHttpPathException();
         }
 
-        Map<HttpRequestMethod,HttpRequestHandler> methodHandler = VALUES.get(path);
+        Map<HttpRequestMethod,HttpRequestHandler> methodHandler = values.get(path);
 
         if (!methodHandler.containsKey(method)) {
             throw new NotAllowedHttpMethodException();
         }
 
-        return VALUES.get(path).get(method);
+        return values.get(path).get(method);
     }
 }
