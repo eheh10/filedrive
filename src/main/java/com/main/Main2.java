59833e8579543ea1925b8071b916c3e5c37600ac
eunhye park <lime1pm@naver.com>
2022-09-14 23:20:14 +0900

    FIX Main2

--- src/main/java/com/main/Main2.java
+++ src/main/java/com/main/Main2.java
@@ -1,6 +1,10 @@
 package com.main;
 
-import com.generator.InputStreamTextGenerator;
+import com.api.HttpRequestBodyFileCreator;
+import com.api.HttpRequestHandlers;
+import com.generator.HttpStringGenerator;
+import com.method.HttpRequestMethod;
+import com.path.HttpRequestPath;
 import com.response.ResponseMsgCreator;
 
 import java.io.BufferedOutputStream;
@@ -16,13 +20,16 @@ public class Main2 {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(7777);
 
+        HttpRequestHandlers handlers = new HttpRequestHandlers();
+        handlers.register(HttpRequestPath.of("/test"), HttpRequestMethod.POST,new HttpRequestBodyFileCreator());
+
         while(true) {
             Socket socket = serverSocket.accept();
 
-            InputStreamTextGenerator generator = InputStreamTextGenerator.of(socket.getInputStream());
+            HttpStringGenerator generator = HttpStringGenerator.of(socket.getInputStream());
             ResponseMsgCreator response = new ResponseMsgCreator();
 
-            String responseMsg = response.create(generator);
+            String responseMsg = response.create(generator,handlers);
             if (responseMsg == null) {
                 continue;
             }
