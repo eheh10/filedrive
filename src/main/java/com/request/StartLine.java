19c1d9ae10865838e21249468fa72e184f0287fb
eunhye park <lime1pm@naver.com>
2022-09-20 03:08:34 +0900

    FIX StartLine

--- src/main/java/com/request/StartLine.java
+++ src/main/java/com/request/StartLine.java
@@ -1,5 +1,8 @@
 package com.request;
 
+import com.exception.InvalidValueException;
+import com.exception.NotAllowedHttpMethodException;
+import com.exception.NullException;
 import com.method.HttpRequestMethod;
 import com.path.HttpRequestPath;
 
@@ -15,13 +18,13 @@ public class StartLine {
 
     private StartLine(HttpRequestMethod method, HttpRequestPath path, String version) {
         if (method==null) {
-            throw new RuntimeException("StarLineParser.HttpRequestMethod is null");
+            throw new NullException("StarLineParser.HttpRequestMethod is null");
         }
         if (path==null) {
-            throw new RuntimeException("StarLineParser.path is null");
+            throw new NullException("StarLineParser.path is null");
         }
         if (version==null) {
-            throw new RuntimeException("StarLineParser.version is null");
+            throw new NullException("StarLineParser.version is null");
         }
 
         this.method = method;
@@ -31,20 +34,25 @@ public class StartLine {
 
     public static StartLine parse(String startLine) {
         if (startLine == null || startLine.isBlank()) {
-            throw new RuntimeException();
+            throw new NullException();
         }
 
         StringTokenizer tokenizer = new StringTokenizer(startLine," ");
         if (tokenizer.countTokens() != 3) {
-            throw new RuntimeException();
+            throw new InvalidValueException();
         }
 
-        HttpRequestMethod method = HttpRequestMethod.valueOf(tokenizer.nextToken());
+        HttpRequestMethod method;
+        try {
+            method = HttpRequestMethod.valueOf(tokenizer.nextToken());
+        } catch (IllegalArgumentException e) {
+            throw new NotAllowedHttpMethodException();
+        }
 
         String pathText = tokenizer.nextToken();
 
         if(!Objects.equals(pathText.charAt(0),'/')) {
-            throw new RuntimeException();
+            throw new InvalidValueException();
         }
 
         if (Objects.equals(method,"GET")) {
@@ -56,7 +64,7 @@ public class StartLine {
         String version = tokenizer.nextToken();
 
         if(version.length() < 6 || !Objects.equals(version.substring(0,5),"HTTP/")) {
-            throw new RuntimeException();
+            throw new InvalidValueException();
         }
 
         return new StartLine(method,path,version);
