815f579037b07bc2e56ef47805d481d45b366943
eunhye park <lime1pm@naver.com>
2022-09-27 01:22:17 +0900

    ADD TemplateNodes

---
+++ src/main/java/com/template/TemplateNodes.java
@@ -0,0 +1,48 @@
+package com.template;
+
+import com.exception.NotFoundKeyException;
+import com.exception.NullException;
+
+import java.util.*;
+
+public class TemplateNodes {
+    private final Map<String,String> values = new HashMap<>();
+    private final Queue<Integer> templateLength = new PriorityQueue<>(Collections.reverseOrder());
+
+    public void register(String templateTxt, String replaceTxt) {
+        if (templateTxt == null || replaceTxt == null) {
+            throw new NullException();
+        }
+
+        values.put(templateTxt, replaceTxt);
+        templateLength.add(templateTxt.length());
+    }
+
+    public String replace(String templateTxt) {
+        if (templateTxt == null) {
+            throw new NullException();
+        }
+
+        if (!values.containsKey(templateTxt)) {
+            throw new NotFoundKeyException();
+        }
+
+        return values.get(templateTxt);
+    }
+
+    public String replaceWithDefault(String templateTxt, String defaultTxt) {
+        if (templateTxt == null) {
+            throw new NullException();
+        }
+
+        if (!values.containsKey(templateTxt)) {
+            return defaultTxt;
+        }
+
+        return values.get(templateTxt);
+    }
+
+    public int getTemplateMaxLength() {
+        return templateLength.peek();
+    }
+}
