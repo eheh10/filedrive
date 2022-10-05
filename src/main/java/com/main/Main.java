fc1da700f0bbc4741147c8881094796bcbc9d52c
eunhye park <lime1pm@naver.com>
2022-10-06 03:22:27 +0900

    FIX Main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -4,6 +4,7 @@ import com.Bootstrap;
 import com.request.HttpRequestMethod;
 import com.request.HttpRequestPath;
 import request.handler.HttpRequestBodyFileCreator;
+import request.handler.HttpRequestFileUploader;
 
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.registerHandler(HttpRequestPath.of("/test"), HttpRequestMethod.POST,new HttpRequestBodyFileCreator());
+        bootstrap.registerHandler(HttpRequestPath.of("/upload"), HttpRequestMethod.POST,new HttpRequestFileUploader());
         bootstrap.start();
     }
 }
