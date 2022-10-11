85b31d90d604109d603fcfe454688d8f733a0ec3
eunhye park <lime1pm@naver.com>
2022-10-11 13:50:11 +0900

    FIX HttpStreamGeneratorTest

--- http/src/test/java/com/HttpStreamGeneratorTest.java
+++ http/src/test/java/com/HttpStreamGeneratorTest.java
@@ -6,8 +6,6 @@ import com.releaser.ResourceCloser;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -17,42 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 class HttpStreamGeneratorTest {
-
-    @ParameterizedTest
-    @ValueSource(strings = {"Hello","Hello World\n","Hello\n World\n\n"})
-    @DisplayName("끝까지 데이터를 읽는지 테스트")
-    void testGenerate(String expected) throws IOException {
-        //given
-        InputStream is = new ByteArrayInputStream(expected.getBytes(StandardCharsets.UTF_8));
-        InputStreamGenerator isGenerator = InputStreamGenerator.of(is);
-        HttpStreamGenerator generator = HttpStreamGenerator.of(isGenerator);
-
-        //when
-        StringBuilder actual = new StringBuilder();
-        while(generator.hasMoreString()) {
-            actual.append(generator.generate());
-        }
-
-        //then
-        Assertions.assertThat(actual.toString()).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"Hello","Hello World\n","Hello\n World\n\n"})
-    @DisplayName("개행문자까지만 데이터를 읽는지 테스트")
-    void testGenerateLine(String str) throws IOException {
-        //given
-        String expected = str.split("\n")[0];
-        InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
-        InputStreamGenerator isGenerator = InputStreamGenerator.of(is);
-        HttpStreamGenerator generator = HttpStreamGenerator.of(isGenerator);
-
-        //when
-        String actual = generator.generateLine();
-
-        //then
-        Assertions.assertThat(actual).isEqualTo(expected);
-    }
 
     @Test
     @DisplayName("복수의 InputStream 연결 테스트")
@@ -76,38 +38,6 @@ class HttpStreamGeneratorTest {
         }
 
         Assertions.assertThat(actual.toString()).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("읽을 값이 더 있는 경우 hasMoreString 테스트")
-    void testHasMoreStringWithData() throws IOException {
-        //given
-        boolean expected = true;
-        InputStream is = new ByteArrayInputStream("data".getBytes(StandardCharsets.UTF_8));
-        InputStreamGenerator isGenerator = InputStreamGenerator.of(is);
-        HttpStreamGenerator generator = HttpStreamGenerator.of(isGenerator);
-
-        //when
-        boolean actual = generator.hasMoreString();
-
-        //then
-        Assertions.assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("읽을 값이 없는 경우 hasMoreString 테스트")
-    void testHasMoreStringWithNoData() throws IOException {
-        //given
-        boolean expected = false;
-        InputStream is = new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8));
-        InputStreamGenerator isGenerator = InputStreamGenerator.of(is);
-        HttpStreamGenerator generator = HttpStreamGenerator.of(isGenerator);
-
-        //when
-        boolean actual = generator.hasMoreString();
-
-        //then
-        Assertions.assertThat(actual).isEqualTo(expected);
     }
 
     @Test
