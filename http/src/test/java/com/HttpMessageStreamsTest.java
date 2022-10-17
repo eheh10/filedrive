15ec5e11833d9ed96df0272531452f3fabc712e7
eunhye park <lime1pm@naver.com>
2022-10-18 01:36:36 +0900

    FIX HttpMessageStreamsTest

--- http/src/test/java/com/HttpMessageStreamsTest.java
+++ http/src/test/java/com/HttpMessageStreamsTest.java
@@ -1,18 +1,14 @@
 package com;
 
 import com.exception.InputNullParameterException;
-import com.releaser.FileResourceCloser;
-import com.releaser.ResourceCloser;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 
 class HttpMessageStreamsTest {
 
@@ -58,30 +54,6 @@ class HttpMessageStreamsTest {
         }
 
         Assertions.assertThat(actual.toString()).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("파일 삭제 releaser 등록시 파일 삭제 테스트")
-    void testRegisterReleaser() throws IOException {
-        //given
-        boolean expected = false;
-        String str = "Hello";
-        InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
-        StringStream stringStream = StringStream.of(is);
-
-        File file = Path.of("src","test","resources","test-delete.txt").toFile();
-        file.createNewFile();
-
-        ResourceCloser closer = new FileResourceCloser(file);
-        HttpMessageStream stream = HttpMessageStream.of(stringStream,closer);
-        HttpMessageStreams streams = HttpMessageStreams.empty().sequenceOf(stream);
-
-        //when
-        streams.close();
-        boolean deleted = file.exists();
-
-        //then
-        Assertions.assertThat(deleted).isEqualTo(expected);
     }
 
     @Test
