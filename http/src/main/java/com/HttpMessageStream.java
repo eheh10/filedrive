94f8f29fc1ca1d9e1db134c3b5ca8e2bc6100168
eunhye park <lime1pm@naver.com>
2022-10-18 01:10:57 +0900

    ADD HttpMessageStream

---
+++ http/src/main/java/com/HttpMessageStream.java
@@ -0,0 +1,56 @@
+package com;
+
+import com.exception.InputNullParameterException;
+import com.exception.NoMoreHttpContentException;
+import com.releaser.NoResourceCloser;
+import com.releaser.ResourceCloser;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class HttpMessageStream implements Closeable {
+    private final StringStream stream;
+    private final ResourceCloser closer;
+
+    private HttpMessageStream(StringStream stream, ResourceCloser closer) {
+        if (stream == null || closer == null) {
+            throw new InputNullParameterException();
+        }
+
+        this.stream = stream;
+        this.closer = closer;
+    }
+
+    public static HttpMessageStream of(StringStream stream) {
+        return new HttpMessageStream(stream,new NoResourceCloser());
+    }
+
+    public static HttpMessageStream of(StringStream stream, ResourceCloser closer) {
+        return new HttpMessageStream(stream,closer);
+    }
+
+    public boolean hasMoreString() throws IOException {
+        return stream.hasMoreString();
+    }
+
+    public String generate() throws IOException {
+        if (!hasMoreString()) {
+            throw new NoMoreHttpContentException();
+        }
+
+        return stream.generate();
+    }
+
+    public String generateLine() throws IOException {
+        if (!hasMoreString()) {
+            throw new NoMoreHttpContentException();
+        }
+
+        return stream.generateLine();
+    }
+
+    @Override
+    public void close() throws IOException {
+        closer.close();
+    }
+}
