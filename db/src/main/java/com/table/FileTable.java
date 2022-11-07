463286782e6170fa2512d9492fa01fc5412bed80
eunhye park <lime1pm@naver.com>
2022-11-07 22:08:19 +0900

    ADD FileTable

---
+++ db/src/main/java/com/table/FileTable.java
@@ -0,0 +1,49 @@
+package com.table;
+
+import com.dto.FileDto;
+import com.exception.InputNullParameterException;
+import com.exception.InsertNullException;
+import com.exception.NoSearchResultException;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class FileTable {
+    private static final Set<FileDto> FILES = new HashSet<>();
+
+    public void insert(FileDto fileDto) {
+        if (fileDto == null) {
+            throw new InsertNullException();
+        }
+        FILES.add(fileDto);
+    }
+
+    public FileDto searchWithFileName(String targetValue) {
+        if (targetValue == null) {
+            throw new InputNullParameterException();
+        }
+
+        for(FileDto fileDto: FILES) {
+            if(Objects.equals(fileDto.getFileName(),targetValue)) {
+                return fileDto;
+            }
+        }
+
+        throw new NoSearchResultException();
+    }
+
+    public FileDto searchWithFilePath(String targetValue) {
+        if (targetValue == null) {
+            throw new InputNullParameterException();
+        }
+
+        for(FileDto fileDto: FILES) {
+            if(Objects.equals(fileDto.getFilePath(),targetValue)) {
+                return fileDto;
+            }
+        }
+
+        throw new NoSearchResultException();
+    }
+}
