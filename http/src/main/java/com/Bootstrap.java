83a074eb1d45171d14d260af9e361b250677b273
eunhye park <lime1pm@naver.com>
2022-11-13 01:37:29 +0900

    FIX Bootstrap

--- http/src/main/java/com/Bootstrap.java
+++ http/src/main/java/com/Bootstrap.java
@@ -9,10 +9,7 @@ import com.request.handler.HttpRequestHandlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -27,43 +24,52 @@ public class Bootstrap {
         handlers.register(path, method, handler);
     }
 
-    public void start() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7777);
+    public void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(7777);
 
-        //임피던스 불일치
-        int headersLimit = Integer.parseInt(finder.find(PropertyKey.HTTP_REQUEST_HEADERS_LENGTH_LIMIT));
-        int bodyLimit = Integer.parseInt(finder.find(PropertyKey.HTTP_REQUEST_HEADERS_LENGTH_LIMIT));
+            int headersLimit = Integer.parseInt(finder.find(PropertyKey.HTTP_REQUEST_HEADERS_LENGTH_LIMIT));
+            int bodyLimit = Integer.parseInt(finder.find(PropertyKey.HTTP_REQUEST_HEADERS_LENGTH_LIMIT));
 
-        HttpLengthLimiter requestHeadersLengthLimit = new HttpLengthLimiter(headersLimit);
-        HttpLengthLimiter requestBodyLengthLimit = new HttpLengthLimiter(bodyLimit);
+            Socket socket = null;
 
-//        HttpLengthLimiter test = HttpLimitLength.REQUEST_BODY.createLimiter();
+            while (true) {
+                try {
+                    HttpLengthLimiter requestHeadersLengthLimit = new HttpLengthLimiter(headersLimit);
+                    HttpLengthLimiter requestBodyLengthLimit = new HttpLengthLimiter(bodyLimit);
 
-        while (true) {
-            Socket socket = serverSocket.accept();
+                    socket = serverSocket.accept();
 
-            OutputStream os = socket.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
+                    OutputStream os = socket.getOutputStream();
+                    BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
 
-            HttpRequestProcessor processor = new HttpRequestProcessor();
+                    HttpRequestProcessor processor = new HttpRequestProcessor();
 
-            try (StringStream isGenerator = StringStream.of(socket.getInputStream());
-                 HttpMessageStreams generator = HttpMessageStreams.of(isGenerator);
-                 HttpMessageStreams responseGenerator = processor.process(generator, handlers, requestHeadersLengthLimit, requestBodyLengthLimit);
-                 OutputStreamWriter bsw = new OutputStreamWriter(bos, StandardCharsets.UTF_8)) {
+                    try (StringStream isGenerator = StringStream.of(socket.getInputStream());
+                         HttpMessageStream generator = HttpMessageStream.of(isGenerator);
+                         HttpMessageStreams responseMsg = processor.process(generator, handlers, requestHeadersLengthLimit, requestBodyLengthLimit);
+                         OutputStreamWriter bsw = new OutputStreamWriter(bos, StandardCharsets.UTF_8)) {
 
-                while (responseGenerator.hasMoreString()) {
-                    String line = responseGenerator.generate();
-                    LOG.debug(line);
-                    bsw.write(line);
+                        while (responseMsg.hasMoreString()) {
+                            String line = responseMsg.generate();
+                            LOG.debug(line);
+                            bsw.write(line);
+                        }
+                        bsw.flush();
+                    } catch (EmptyRequestException e) {
+                        continue;
+                    }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (socket == null) {
+                        continue;
+                    }
+                    socket.close();
                 }
-
-                bsw.flush();
-            } catch (EmptyRequestException e) {
-                continue;
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
