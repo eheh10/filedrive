b8b29751fffa06e19c8478b3629017272bf555e7
eunhye park <lime1pm@naver.com>
2022-11-13 01:29:49 +0900

    FIX Main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -3,18 +3,29 @@ package com.main;
 import com.Bootstrap;
 import com.request.HttpRequestMethod;
 import com.request.HttpRequestPath;
-import request.handler.HttpRequestBodyFileCreator;
-import request.handler.HttpRequestFileUploader;
+import com.request.handler.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
+        boolean requiredLogin = true;
+
         Bootstrap bootstrap = new Bootstrap();
+
+        bootstrap.registerHandler(HttpRequestPath.of("/signUp"), HttpRequestMethod.POST, new HttpRequestUserCreator());
+        bootstrap.registerHandler(HttpRequestPath.of("/login"), HttpRequestMethod.POST, new HttpRequestUserFinder());
         bootstrap.registerHandler(HttpRequestPath.of("/upload"), HttpRequestMethod.POST, new HttpRequestFileUploader());
-        bootstrap.registerHandler(HttpRequestPath.of("/download"), HttpRequestMethod.POST, new HttpRequestFileDownloader());
+        bootstrap.registerHandler(HttpRequestPath.of("/download"), HttpRequestMethod.POST, new HttpRequestLoginDecorator(new HttpRequestFileUploader()));
+        bootstrap.registerHandler(HttpRequestPath.of("/page/login"), HttpRequestMethod.GET, new HttpRequestPageStream(!requiredLogin));
+        bootstrap.registerHandler(HttpRequestPath.of("/page/upload"), HttpRequestMethod.GET, new HttpRequestPageStream(requiredLogin));
         bootstrap.registerHandler(HttpRequestPath.ofResourcePath(), HttpRequestMethod.GET, new HttpResourceStream());
 
         bootstrap.start();
     }
+
 }
