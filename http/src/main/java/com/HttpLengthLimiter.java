af949b3c34e69abd2c6ef74d121f0ac8af0823d2
eunhye park <lime1pm@naver.com>
2022-11-13 01:43:26 +0900

    FIX HttpLengthLimiter

--- http/src/main/java/com/HttpLengthLimiter.java
+++ http/src/main/java/com/HttpLengthLimiter.java
@@ -1,28 +1,23 @@
 package com;
 
 import com.exception.ExceedingHttpLengthLimitException;
-import com.exception.NotPositiveNumberException;
+import com.exception.MustBePositiveNumberException;
 
 public class HttpLengthLimiter {
-//    public static final int HTTP_REQUEST_BODY_LIMIT_LENGTH = 100;
-//    public static final int HTTP_RESPONSE_BODY_LIMIT_LENGTH = 100;
-
     private int lengthSum = 0;
     private final int limitLength;
 
     public HttpLengthLimiter(int limitLength) {
         if (0 >= limitLength) {
-            throw new NotPositiveNumberException();
+            throw new MustBePositiveNumberException();
         }
 
         this.limitLength = limitLength;
     }
 
-//    public static HttpLengthLimiter Http()
-
     public void accumulate(int length) {
         if (length < 0) {
-            throw new NotPositiveNumberException();
+            throw new MustBePositiveNumberException();
         }
 
         lengthSum += length;
