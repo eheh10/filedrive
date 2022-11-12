5211f40f677835cdda4306606596068d4990870c
eunhye park <lime1pm@naver.com>
2022-11-13 01:46:58 +0900

    FIX HttpRequestHandler parameter

--- http/src/main/java/com/request/handler/HttpRequestHandler.java
+++ http/src/main/java/com/request/handler/HttpRequestHandler.java
@@ -1,5 +1,6 @@
 package com.request.handler;
 
+import com.HttpMessageStream;
 import com.HttpMessageStreams;
 import com.header.HttpHeaders;
 import com.request.HttpRequestPath;
@@ -7,5 +8,5 @@ import com.request.HttpRequestPath;
 import java.io.IOException;
 
 public interface HttpRequestHandler {
-    HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, HttpMessageStreams bodyStream) throws IOException;
+    HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, HttpMessageStream bodyStream) throws IOException;
 }
