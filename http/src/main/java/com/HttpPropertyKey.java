dda5ec2af1fe5752adc50c2c932452cff3d7a3a0
eunhye park <lime1pm@naver.com>
2022-11-23 04:32:35 +0900

    FIX HttpPropertyKey

--- http/src/main/java/com/HttpPropertyKey.java
+++ http/src/main/java/com/HttpPropertyKey.java
@@ -2,13 +2,14 @@ package com;
 
 import com.exception.InputNullParameterException;
 
-public enum PropertyKey {
+public enum HttpPropertyKey {
     HTTP_REQUEST_HEADERS_LENGTH_LIMIT("http_request_headers_length_limit"),
     HTTP_REQUEST_BODY_LENGTH_LIMIT("http_request_body_length_limit"),
-    NOT_ALLOWED_EXTENSION("not_allowed_extension");
+    NOT_ALLOWED_FILE_EXTENSION("not_allowed_file_extension"),
+    HTTP_REQUEST_PROCESS_THREAD_POOL_COUNT("http_request_process_thread_pool_count");
 
     private final String value;
-    PropertyKey(String value) {
+    HttpPropertyKey(String value) {
         if (value == null) {
             throw new InputNullParameterException();
         }
