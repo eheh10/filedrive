587995d5b008c7cff604473f9da633a10a2212d2
eunhye park <lime1pm@naver.com>
2022-11-26 02:16:20 +0900

    FIX HttpRequestPagePath

--- src/main/java/com/request/HttpRequestPagePath.java
+++ src/main/java/com/request/HttpRequestPagePath.java
@@ -6,7 +6,8 @@ public enum HttpRequestPagePath {
     MAIN(Paths.get("src","main","resources","main.html").toString()),
     SIGN_UP(Paths.get("src","main","resources","signUp.html").toString()),
     LOGIN(Paths.get("src","main","resources","login.html").toString()),
-    UPLOAD(Paths.get("src","main","resources","upload.html").toString());
+    UPLOAD(Paths.get("src","main","resources","upload.html").toString()),
+    DOWNLOAD(Paths.get("src","main","resources","download.html").toString());
 
     private final String resourcePath;
     HttpRequestPagePath(String resourcePath) {
