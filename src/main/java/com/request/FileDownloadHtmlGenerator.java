996bfc4b7be574210e03141004602bc36c1e54c5
eunhye park <lime1pm@naver.com>
2022-11-26 02:02:51 +0900

    ADD FileDownloadHtmlGenerator

---
+++ src/main/java/com/request/FileDownloadHtmlGenerator.java
@@ -0,0 +1,41 @@
+package com.request;
+
+import com.dto.FileDto;
+import com.exception.InputNullParameterException;
+
+import java.util.Set;
+
+public class FileDownloadHtmlGenerator {
+    private final Set<FileDto> files;
+
+    private FileDownloadHtmlGenerator(Set<FileDto> files) {
+        if (files == null) {
+            throw new InputNullParameterException();
+        }
+        this.files = files;
+    }
+
+    public static FileDownloadHtmlGenerator of(Set<FileDto> files) {
+        if (files == null) {
+            throw new InputNullParameterException();
+        }
+        return new FileDownloadHtmlGenerator(files);
+    }
+
+    public String generate() {
+        StringBuilder html = new StringBuilder();
+
+        for(FileDto fileDto:files) {
+            String fileName = fileDto.getName();
+
+            html.append("<form action=\"http://localhost:7777/download\" method=\"post\">")
+                    .append("<p>")
+                    .append("<input type=\"text\" name=\"fileName\" value=\"").append(fileName).append("\" readonly>")
+                    .append("<input type=\"submit\" value=\"").append("다운로드\">")
+                    .append("</p>\n")
+                    .append("</form>");
+        }
+
+        return html.toString();
+    }
+}
