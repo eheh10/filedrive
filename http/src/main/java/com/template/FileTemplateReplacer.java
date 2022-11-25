aedeee878bed1e278e86cb551967cd636f8bb87c
eunhye park <lime1pm@naver.com>
2022-11-26 02:20:04 +0900

    FIX FileTemplateReplacer

--- http/src/main/java/com/template/FileTemplateReplacer.java
+++ http/src/main/java/com/template/FileTemplateReplacer.java
@@ -1,12 +1,10 @@
 package com.template;
 
-import com.TemplateFileStream;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
-public class FileTemplateReplacer implements Closeable{
+public class FileTemplateReplacer implements Closeable {
     private final TemplateFileStream templateFileStream;
     private final OutputStreamWriter osw;
 
@@ -18,62 +16,73 @@ public class FileTemplateReplacer implements Closeable{
         this.osw = osw;
     }
 
-    public static FileTemplateReplacer of(TemplateFileStream templateFileStream, Path replacedFileName) throws FileNotFoundException {
-        OutputStream os = new FileOutputStream(replacedFileName.toFile());
+    public static FileTemplateReplacer of(TemplateFileStream templateFileStream, Path replacedFileName) {
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream(replacedFileName.toFile());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
         OutputStreamWriter osw = new OutputStreamWriter(bos,StandardCharsets.UTF_8);
 
         return new FileTemplateReplacer(templateFileStream,osw);
     }
 
-    public void replace(TemplateNodes templateNodes, TemplateText template) throws IOException {
-        StringBuilder foundTemplateText = new StringBuilder();
+    public void replace(TemplateNodes templateNodes, TemplateText template) {
+        try {
+            StringBuilder foundTemplateText = new StringBuilder();
 
-        String startTemplate = template.getStart();
-        String endTemplate = template.getEnd();
-        int txtMaxLength = templateNodes.getTemplateMaxLength();
-        int templateLength = startTemplate.length() + endTemplate.length();
+            String startTemplate = template.getStart();
+            String endTemplate = template.getEnd();
 
-        while(templateFileStream.hasMoreFileContent()) {
-            String fileContent = templateFileStream.generate();
+            int txtMaxLength = templateNodes.getTemplateMaxLength();
+            int templateLength = startTemplate.length() + endTemplate.length();
+
+            while (templateFileStream.hasMoreFileContent()) {
+                String fileContent = templateFileStream.generate();
+
+                if (foundTemplateText.length() != 0) {
+                    if (foundTemplateText.length() < txtMaxLength + templateLength) {
+                        foundTemplateText.append(fileContent);
+                        continue;
+                    }
+
+                    String replace = replaceTemplate(foundTemplateText, startTemplate, endTemplate, 0, txtMaxLength, templateNodes);
+
+                    int nextStartIdx = foundTemplateText.indexOf(startTemplate.substring(0, 1));
+                    if (nextStartIdx != -1) {
+                        osw.write(replace.substring(0, nextStartIdx));
+                        foundTemplateText.setLength(0);
+                        foundTemplateText.append(replace.substring(nextStartIdx));
+                        foundTemplateText.append(fileContent);
+                        continue;
+                    }
+
+                    osw.write(replace);
+                    foundTemplateText.setLength(0);
+                }
+
+                int startIdx = fileContent.indexOf(startTemplate.charAt(0));
+                if (startIdx == -1) {
+                    osw.write(fileContent);
+                    continue;
+                }
+
+                osw.write(fileContent.substring(0, startIdx));
+                foundTemplateText.append(fileContent.substring(startIdx));
+            }
 
             if (foundTemplateText.length() != 0) {
-                if (foundTemplateText.length() < txtMaxLength+templateLength) {
-                    foundTemplateText.append(fileContent);
-                    continue;
-                }
-
-                String replace = replaceTemplate(foundTemplateText,startTemplate,endTemplate,0,txtMaxLength,templateNodes);
-
-                int nextStartIdx = foundTemplateText.indexOf(startTemplate.substring(0,1));
-                if (nextStartIdx != -1) {
-                    osw.write(replace.substring(0,nextStartIdx));
-                    foundTemplateText.setLength(0);
-                    foundTemplateText.append(replace.substring(nextStartIdx));
-                    foundTemplateText.append(fileContent);
-                    continue;
-                }
-
+                String replace = replaceTemplate(foundTemplateText, startTemplate, endTemplate, 0, txtMaxLength, templateNodes);
                 osw.write(replace);
-                foundTemplateText.setLength(0);
             }
 
-            int startIdx = fileContent.indexOf(startTemplate.charAt(0));
-            if (startIdx == -1) {
-                osw.write(fileContent);
-                continue;
-            }
-
-            osw.write(fileContent.substring(0,startIdx));
-            foundTemplateText.append(fileContent.substring(startIdx,fileContent.length()-startIdx));
+            osw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        if (foundTemplateText.length() != 0) {
-            String replace = replaceTemplate(foundTemplateText,startTemplate,endTemplate,0,txtMaxLength,templateNodes);
-            osw.write(replace);
-        }
-
-        osw.flush();
     }
 
     private String replaceTemplate(StringBuilder templateTxt, String startTemplate, String endTemplate, int initialIdx, int max, TemplateNodes replaceTxt) {
@@ -101,8 +110,13 @@ public class FileTemplateReplacer implements Closeable{
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         templateFileStream.close();
-        osw.close();
+
+        try {
+            osw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
