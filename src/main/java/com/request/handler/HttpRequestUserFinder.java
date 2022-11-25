bb3ca7e390fea85618141ed23e5de35904c0e829
eunhye park <lime1pm@naver.com>
2022-11-26 02:13:17 +0900

    FIX HttpRequestUserFinder

--- src/main/java/com/request/handler/HttpRequestUserFinder.java
+++ src/main/java/com/request/handler/HttpRequestUserFinder.java
@@ -1,25 +1,27 @@
 package com.request.handler;
 
-import com.*;
+import com.HttpMessageStreams;
+import com.HttpRequestLengthLimiters;
+import com.RetryHttpRequestStream;
 import com.dto.UserDto;
 import com.exception.InputNullParameterException;
+import com.exception.InvalidHttpRequestInputException;
 import com.exception.NotFoundQueryStringValueException;
 import com.header.HttpHeaders;
 import com.request.HttpRequestPath;
-import com.table.UserTable;
+import com.response.HttpResponseStatus;
+import com.table.SessionStorage;
+import com.table.Users;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 public class HttpRequestUserFinder implements HttpRequestHandler {
-    private final UserTable userTable = new UserTable();
+    private final Users users = new Users();
     private final SessionStorage sessionStorage = new SessionStorage();
 
     @Override
-    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, HttpMessageStream bodyStream) throws IOException {
+    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestLengthLimiters requestLengthLimiters) {
         if (httpRequestPath == null || httpHeaders == null || bodyStream == null) {
             throw new InputNullParameterException();
         }
@@ -34,40 +36,21 @@ public class HttpRequestUserFinder implements HttpRequestHandler {
         String userIdValue = searchValue(queryString.toString(), "id");
         String userPwdValue = searchValue(queryString.toString(), "password");
 
-        UserDto userDto = UserDto.builder()
-                .id(userIdValue)
-                .pwd(userPwdValue)
-                .build();
-
-        if (userTable.IsUnregisteredUser(userDto)) {
-            response.append("HTTP/1.1 ")
-                    .append(HttpResponseStatus.CODE_200.code()).append(" ")
-                    .append(HttpResponseStatus.CODE_200.message()).append("\n")
-                    .append("Location: http://localhost:7777/page/login\n");
-
-            InputStream responseIs = new ByteArrayInputStream(response.toString().getBytes(StandardCharsets.UTF_8));
-            StringStream responseStream = StringStream.of(responseIs);
-
-            return HttpMessageStreams.of(responseStream);
+        UserDto loginUser = users.find_BY_ID_PWD(userIdValue,userPwdValue);
+        if (loginUser == null) {
+            throw new InvalidHttpRequestInputException();
         }
 
-        Cookies cookies = new Cookies();
-        cookies.register("userId", userIdValue);
-        cookies.register("userPwd", userPwdValue);
-
-        String sessionId = sessionStorage.createSession(cookies);
+        String sessionId = sessionStorage.createSession(loginUser);
 
         response.append("HTTP/1.1 ")
                 .append(HttpResponseStatus.CODE_303.code()).append(" ")
                 .append(HttpResponseStatus.CODE_303.message()).append("\n")
                 .append("Location: http://localhost:7777/page/upload\n")
-                .append("Set-Cookie: ").append(SessionStorage.SESSION_ID_NAME).append("=").append(sessionId)
+                .append("Set-Cookie: ").append(SessionStorage.SESSION_FIELD_NAME).append("=").append(sessionId)
                 .append("\n");
 
-        InputStream responseIs = new ByteArrayInputStream(response.toString().getBytes(StandardCharsets.UTF_8));
-        StringStream responseStream = StringStream.of(responseIs);
-
-        return HttpMessageStreams.of(responseStream);
+        return HttpMessageStreams.of(response.toString());
     }
 
     private String searchValue(String query, String target) {
