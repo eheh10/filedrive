6d9708dd612099dfecb1ae9ea0205d689493330b
eunhye park <lime1pm@naver.com>
2022-11-26 02:03:31 +0900

    ADD FileDownloadPageStream

---
+++ src/main/java/com/request/handler/FileDownloadPageStream.java
@@ -0,0 +1,113 @@
+package com.request.handler;
+
+import com.*;
+import com.dto.FileDto;
+import com.exception.InputNullParameterException;
+import com.exception.InvalidSessionException;
+import com.header.HttpHeaderField;
+import com.header.HttpHeaders;
+import com.releaser.FileResourceCloser;
+import com.releaser.ResourceCloser;
+import com.request.FileDownloadHtmlGenerator;
+import com.request.HttpRequestPagePath;
+import com.request.HttpRequestPath;
+import com.response.HttpResponseStatus;
+import com.table.SessionStorage;
+import com.table.UserFiles;
+import com.template.FileTemplateReplacer;
+import com.template.TemplateFileStream;
+import com.template.TemplateNodes;
+import com.template.TemplateText;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Set;
+
+public class FileDownloadPageStream implements HttpRequestHandler {
+    private final SessionStorage sessionStorage = new SessionStorage();
+    private final UserFiles userFiles = new UserFiles();
+
+    @Override
+    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestLengthLimiters requestLengthLimiters) {
+        if (httpRequestPath == null || httpHeaders == null || bodyStream == null) {
+            throw new InputNullParameterException();
+        }
+
+        try {
+            // 파일 리스트 가공
+            HttpHeaderField cookie = httpHeaders.findProperty("Cookie");
+            String sessionId = searchSessionId(cookie);
+
+            int userNum = sessionStorage.getUserNum(sessionId);
+            Set<FileDto> files = userFiles.filesOf(userNum);
+
+            // 파일 리스트 html 가공
+            FileDownloadHtmlGenerator htmlGenerator = FileDownloadHtmlGenerator.of(files);
+            String fileListHtml = htmlGenerator.generate();
+
+            // 템플릿에 파일 리스트 대치
+            TemplateNodes templateNodes = new TemplateNodes();
+            templateNodes.register("fileList",fileListHtml);
+
+            String pagePath = HttpRequestPagePath.DOWNLOAD.path();
+            InputStream pageIs = new FileInputStream(pagePath);
+            TemplateFileStream templateFile = TemplateFileStream.of(pageIs);
+            Path replacedFile = Path.of("src","main","resources", "fileListPage.html");
+
+            try (FileTemplateReplacer replacer = FileTemplateReplacer.of(templateFile,replacedFile);
+            ) {
+                replacer.replace(templateNodes, TemplateText.ERROR_TEMPLATE);
+            }
+
+            // http response message 가공
+            StringBuilder response = new StringBuilder();
+
+            response.append("HTTP/1.1 ")
+                    .append(HttpResponseStatus.CODE_200.code()).append(" ")
+                    .append(HttpResponseStatus.CODE_200.message()).append("\n")
+                    .append("Content-Type: text/html;charset=UTF-8\n\n");
+
+            HttpMessageStreams responseMsg = HttpMessageStreams.of(response.toString());
+
+            ResourceCloser releaser = new FileResourceCloser(replacedFile.toFile());
+            InputStream bodyInput = new FileInputStream(replacedFile.toFile());
+            HttpMessageStream pageHtml = HttpMessageStream.of(bodyInput,releaser);
+
+            return responseMsg.sequenceOf(pageHtml);
+        } catch (FileNotFoundException e) {
+            return createRedirectionResponse(HttpResponseStatus.CODE_500);
+        }
+    }
+
+    private String searchSessionId(HttpHeaderField cookie) {
+        for(String value:cookie.getValues()) {
+            int delIdx = value.indexOf('=');
+            if (delIdx == -1) {
+                continue;
+            }
+
+            if (Objects.equals(value.substring(0,delIdx), SessionStorage.SESSION_FIELD_NAME)) {
+                return value.substring(delIdx+1);
+            }
+        }
+        throw new InvalidSessionException();
+    }
+
+    private HttpMessageStreams createRedirectionResponse(HttpResponseStatus status) {
+        StringBuilder response = new StringBuilder();
+
+        response.append("HTTP/1.1 ")
+                .append(status.code()).append(" ")
+                .append(status.message()).append("\n");
+
+        InputStream responseIs = new ByteArrayInputStream(response.toString().getBytes(StandardCharsets.UTF_8));
+        StringStream responseStream = StringStream.of(responseIs);
+
+        return HttpMessageStreams.of(responseStream);
+    }
+}
