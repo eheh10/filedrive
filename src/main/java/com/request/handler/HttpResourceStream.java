110b83b66c3cef5631640abc4071fb15e4d75b8e
eunhye park <lime1pm@naver.com>
2022-12-07 18:33:52 +0900

    FIX HttpResourceStream

--- src/main/java/com/request/handler/HttpResourceStream.java
+++ src/main/java/com/request/handler/HttpResourceStream.java
@@ -4,20 +4,21 @@ import com.http.*;
 import com.http.exception.InputNullParameterException;
 import com.http.header.HttpHeaders;
 import com.http.request.HttpRequestPath;
+import com.http.request.HttpRequestQueryString;
 import com.http.request.handler.HttpRequestHandler;
 import com.http.response.HttpResponseStatus;
 
 public class HttpResourceStream implements HttpRequestHandler {
-    private final ResourceFinder resourceFinder = new ResourceFinder();
-    private final HttpRequestPath defaultPath = HttpRequestPath.of("/images");
+    private static final ResourceFinder RESOURCE_FINDER = new ResourceFinder();
+    private static final HttpRequestPath DEFAULT_PATH = HttpRequestPath.of("/images");
 
     @Override
-    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestLengthLimiters requestLengthLimiters) {
+    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
         if (httpRequestPath == null || httpHeaders == null || bodyStream == null) {
             throw new InputNullParameterException();
         }
 
-        HttpRequestPath requestFilePath = defaultPath.combine(httpRequestPath);
+        HttpRequestPath requestFilePath = DEFAULT_PATH.combine(httpRequestPath);
 
         StringBuilder response = new StringBuilder();
 
@@ -27,7 +28,7 @@ public class HttpResourceStream implements HttpRequestHandler {
                 .append("Content-Type: ").append(requestFilePath.contentType()).append("\n\n");
 
         HttpMessageStreams responseMsg = HttpMessageStreams.of(response.toString());
-        HttpMessageStream fileStream = resourceFinder.findGetRequestResource(requestFilePath);
+        HttpMessageStream fileStream = RESOURCE_FINDER.findGetRequestResource(requestFilePath);
 
         return responseMsg.sequenceOf(fileStream);
     }
