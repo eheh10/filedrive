ccb4b4dec8a1beebd7abe99c60b69d03dce03f92
eunhye park <lime1pm@naver.com>
2022-12-07 18:15:12 +0900

    ADD TokenDto, UserInfoDto

---
+++ api/src/main/java/com/api/dto/TokenDto.java
@@ -0,0 +1,47 @@
+package com.api.dto;
+
+import com.api.exception.InputNullParameterException;
+import lombok.Builder;
+import lombok.ToString;
+
+@ToString
+public class TokenDto {
+    private final String accessToken;
+    private final String scope;
+    private final String token_type;
+    private final String id_token;
+    private final String refresh_token;
+
+    @Builder
+    public TokenDto(String accessToken, String scope, String token_type, String id_token, String refresh_token) {
+        if (accessToken==null || scope==null || token_type==null || id_token==null || refresh_token==null) {
+            throw new InputNullParameterException();
+        }
+
+        this.accessToken = accessToken;
+        this.scope = scope;
+        this.token_type = token_type;
+        this.id_token = id_token;
+        this.refresh_token = refresh_token;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public String getId_token() {
+        return id_token;
+    }
+
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+}
---
+++ api/src/main/java/com/api/dto/UserInfoDto.java
@@ -0,0 +1,28 @@
+package com.api.dto;
+
+import com.api.exception.InputNullParameterException;
+import lombok.Builder;
+import lombok.ToString;
+
+@ToString
+public class UserInfoDto {
+    private final String snsUid;
+    private final String snsEmail;
+
+    @Builder
+    public UserInfoDto(String snsUid, String snsEmail) {
+        if (snsUid==null || snsEmail==null) {
+            throw new InputNullParameterException();
+        }
+        this.snsUid = snsUid;
+        this.snsEmail = snsEmail;
+    }
+
+    public String getSnsUid() {
+        return snsUid;
+    }
+
+    public String getSnsEmail() {
+        return snsEmail;
+    }
+}
