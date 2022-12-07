5c94e82e950044d077c604d0736aa4d967cd2f3b
eunhye park <lime1pm@naver.com>
2022-12-07 18:27:56 +0900

    FIX FileDownloads
    version 필드 추가
    getDownloadVersion() 추가

--- db/src/main/java/com/db/table/FileDownloads.java
+++ db/src/main/java/com/db/table/FileDownloads.java
@@ -1,8 +1,10 @@
 package com.db.table;
 
 import com.db.DbConnector;
+import com.db.DbManualConnector;
 import com.db.dto.FileDownloadDto;
 import com.db.exception.InputNullParameterException;
+import com.db.exception.VersionUpdatedException;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -13,9 +15,11 @@ import java.time.format.DateTimeFormatter;
 
 public class FileDownloads {
     private static final DbConnector CONNECTOR = DbConnector.getInstance();
+    private static final DbManualConnector MANUAL_CONNECTOR = DbManualConnector.getInstance();
     private static final PreparedStatement INSERT_DOWNLOAD = CONNECTOR.preparedSql("INSERT INTO downloads(uid,user_uid,download_date,count) VALUES (?,?,?,?)");
-    private static final PreparedStatement UPDATE_COUNT = CONNECTOR.preparedSql("UPDATE downloads SET count=? WHERE uid=?");
-    private static final PreparedStatement SEARCH_COUNT = CONNECTOR.preparedSql("SELECT * FROM downloads WHERE user_uid=? and download_date=?");
+    private static final PreparedStatement UPDATE_COUNT = MANUAL_CONNECTOR.preparedSql("UPDATE downloads SET count=?,version=? WHERE uid=?");
+    private static final PreparedStatement SEARCH_DOWNLOAD = CONNECTOR.preparedSql("SELECT * FROM downloads WHERE user_uid=? and download_date=?");
+    private static final PreparedStatement SEARCH_VERSION = CONNECTOR.preparedSql("SELECT version FROM downloads WHERE uid=?");
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_DATE;
     private static ResultSet resultSet = null;
 
@@ -44,10 +48,35 @@ public class FileDownloads {
         }
 
         try {
+            int dbVersion = getDownloadVersion(fileDownloadDto.getUid());
+
             UPDATE_COUNT.setInt(1, fileDownloadDto.getCount()+1);
-            UPDATE_COUNT.setString(2, fileDownloadDto.getUid());
+            UPDATE_COUNT.setInt(2, fileDownloadDto.getVersion()+1);
+            UPDATE_COUNT.setString(3, fileDownloadDto.getUid());
 
             UPDATE_COUNT.executeUpdate();
+
+            if (fileDownloadDto.getVersion() != dbVersion) {
+                MANUAL_CONNECTOR.rollback();
+                throw new VersionUpdatedException();
+            }
+
+            MANUAL_CONNECTOR.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Integer getDownloadVersion(String fileUid) {
+        try {
+            SEARCH_VERSION.setString(1, fileUid);
+
+            resultSet = SEARCH_VERSION.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+
+            return resultSet.getInt("version");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -61,10 +90,10 @@ public class FileDownloads {
         Date formattedDate = Date.valueOf(downloadDate.format(DATE_FORMATTER));
 
         try {
-            SEARCH_COUNT.setString(1, userUid);
-            SEARCH_COUNT.setDate(2, formattedDate);
+            SEARCH_DOWNLOAD.setString(1, userUid);
+            SEARCH_DOWNLOAD.setDate(2, formattedDate);
 
-            resultSet = SEARCH_COUNT.executeQuery();
+            resultSet = SEARCH_DOWNLOAD.executeQuery();
             if (!resultSet.next()) {
                 return null;
             }
@@ -74,6 +103,7 @@ public class FileDownloads {
                     .userUid(resultSet.getString("user_uid"))
                     .downloadDate(resultSet.getDate("download_date").toLocalDate())
                     .count(resultSet.getInt("count"))
+                    .version(resultSet.getInt("version"))
                     .build();
         } catch (SQLException e) {
             throw new RuntimeException(e);
