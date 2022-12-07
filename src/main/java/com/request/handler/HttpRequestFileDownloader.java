151a5491118e919da1e9918081612b460ffe3bb9
eunhye park <lime1pm@naver.com>
2022-12-08 04:16:27 +0900

    FIX HttpRequestFileDownloader
    zip 압축

--- src/main/java/com/request/handler/HttpRequestFileDownloader.java
+++ src/main/java/com/request/handler/HttpRequestFileDownloader.java
@@ -11,9 +11,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.http.*;
 import com.http.exception.InputNullParameterException;
+import com.http.exception.InvalidHttpRequestInputException;
 import com.http.exception.NotFoundHttpHeadersPropertyException;
 import com.http.exception.NotFoundHttpRequestFileException;
-import com.http.exception.NotFoundQueryStringValueException;
 import com.http.header.HttpHeaderField;
 import com.http.header.HttpHeaders;
 import com.http.request.HttpRequestPath;
@@ -28,9 +28,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class HttpRequestFileDownloader implements HttpRequestHandler {
     private static final Path DEFAULT_PATH = Paths.get("src","main","resources","uploaded-file");
@@ -39,6 +40,7 @@ public class HttpRequestFileDownloader implements HttpRequestHandler {
     private static final UserFiles USER_FILES = new UserFiles();
     private static final FileDownloads FILE_DOWNLOADS = new FileDownloads();
     private static final JsonMapper JSON_MAPPER = new JsonMapper();
+    private static final String FILE_FILED_NAME = "fileName";
 
     @Override
     public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
@@ -57,17 +59,18 @@ public class HttpRequestFileDownloader implements HttpRequestHandler {
 
         String userUid = SESSION_STORAGE.getUserUid(sessionId);
 
-        String fileName = searchFileName(bodyQueryString.toString(), "fileName");
-        FileDto foundFile = USER_FILES.searchFile(fileName,userUid);
-        String targetPath = foundFile.getPath();
-        File targetFile = DEFAULT_PATH.resolve(targetPath).toFile();
+        List<File> downloadFiles = searchFiles(bodyQueryString.toString(),FILE_FILED_NAME,userUid);
 
-        if (!targetFile.exists()) {
-            throw new NotFoundHttpRequestFileException();
+        if (downloadFiles.size() == 0) {
+            throw new InvalidHttpRequestInputException();
         }
 
+        boolean isZipFile = downloadFiles.size() > 1;
+
+        File responseFile = isZipFile? zippingFiles(downloadFiles) : downloadFiles.get(0);
+
         try {
-            recordDownload(userUid);
+            recordDownload(userUid,downloadFiles.size());
         } catch (DownloadCountLimitExceededException e) {
             StringBuilder response = new StringBuilder();
 
@@ -91,22 +94,95 @@ public class HttpRequestFileDownloader implements HttpRequestHandler {
         StringBuilder response = new StringBuilder();
         response.append("HTTP/1.1 ")
                 .append(HttpResponseStatus.CODE_200.code()).append(" ")
-                .append(HttpResponseStatus.CODE_200.message()).append("\n")
-                .append("Content-Type: application/octet-stream;charset=euc-kr\n")
-                .append("Content-Disposition: attachment; filename=\"")
-                .append(fileName)
+                .append(HttpResponseStatus.CODE_200.message()).append("\n");
+
+        if (isZipFile) {
+            response.append("Content-Type: application/zip;charset=euc-kr\n");
+        } else {
+            response.append("Content-Type: application/octet-stream;charset=euc-kr\n");
+        }
+
+        response.append("Content-Disposition: attachment; filename=\"")
+                .append(responseFile.getName())
                 .append("\"\n\n");
 
         HttpMessageStreams responseMsg = HttpMessageStreams.of(response.toString());
 
         try {
-            InputStream fileIs = new FileInputStream(targetFile);
-            HttpMessageStream responseBody = HttpMessageStream.of(fileIs);
+            InputStream fileIs = new FileInputStream(responseFile);
+            InputStream responseIs = isZipFile? fileIs: new ZipInputStream(fileIs);
+            HttpMessageStream responseBody = HttpMessageStream.of(responseIs);
 
             return responseMsg.sequenceOf(responseBody);
         } catch (FileNotFoundException e) {
             return createRedirectionResponse(HttpResponseStatus.CODE_500);
         }
+    }
+
+    private List<File> searchFiles(String queryString, String fieldName, String userUid) {
+        List<File> downloadFiles = new ArrayList<>();
+
+        for(String field : queryString.split("&")) {
+            int startIdx = field.indexOf(fieldName + "=");
+
+            if (startIdx == -1) {
+                continue;
+            }
+
+            String fileName = URLDecoder.decode(
+                    field.substring(startIdx + fieldName.length() + 1),
+                    StandardCharsets.UTF_8);
+
+            FileDto foundFile = USER_FILES.searchFile(fileName,userUid);
+            String targetPath = foundFile.getPath();
+            File targetFile = DEFAULT_PATH.resolve(targetPath).toFile();
+
+            if (!targetFile.exists()) {
+                throw new NotFoundHttpRequestFileException();
+            }
+
+            downloadFiles.add(targetFile);
+        }
+
+        return Collections.unmodifiableList(downloadFiles);
+    }
+
+    private File zippingFiles(List<File> downloadFiles) {
+        File zipFile = new File(DEFAULT_PATH.toFile(), "files.zip");
+
+        try {
+            OutputStream zipOs = new FileOutputStream(zipFile);
+            BufferedOutputStream zipBos = new BufferedOutputStream(zipOs,8192);
+            ZipOutputStream zipOut = new ZipOutputStream(zipBos,StandardCharsets.UTF_8);
+
+            InputStream fileIs;
+            BufferedInputStream fileBis;
+            byte[] buffer = new byte[8192];
+            int len = -1;
+
+            for (File file : downloadFiles) {
+                ZipEntry ze = new ZipEntry(file.getName());
+                zipOut.putNextEntry(ze);
+
+                fileIs = new FileInputStream(file);
+                fileBis = new BufferedInputStream(fileIs,8192);
+
+                while ((len = fileBis.read(buffer)) != -1) {
+                    zipOut.write(buffer, 0, len);
+                }
+
+                zipOut.closeEntry();
+                fileBis.close();
+            }
+            zipOut.flush();
+            zipOut.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return zipFile;
     }
 
     private String convertJson(Map mapValue) {
@@ -117,7 +193,7 @@ public class HttpRequestFileDownloader implements HttpRequestHandler {
         }
     }
 
-    private void recordDownload(String userUid) {
+    private void recordDownload(String userUid, int numberOfFiles) {
         LocalDate today = LocalDate.now();
         FileDownloadDto todayDownload = FILE_DOWNLOADS.searchDownload(userUid,today);
 
@@ -133,37 +209,25 @@ public class HttpRequestFileDownloader implements HttpRequestHandler {
             FILE_DOWNLOADS.insert(todayDownload);
         }
 
-        checkDownloadLimit(todayDownload);
+        checkDownloadLimit(todayDownload,numberOfFiles);
 
         int retry = 3;
         while (retry-- > 0){
             try {
-                FILE_DOWNLOADS.countDownload(todayDownload);
+                FILE_DOWNLOADS.countDownload(todayDownload,numberOfFiles);
                 break;
             } catch (VersionUpdatedException e) {
                 todayDownload = FILE_DOWNLOADS.searchDownload(userUid, today);
-                checkDownloadLimit(todayDownload);
+                checkDownloadLimit(todayDownload,numberOfFiles);
                 continue;
             }
         }
     }
 
-    private void checkDownloadLimit(FileDownloadDto todayDownload) {
-        if (todayDownload.getCount() + 1 > PROPERTY_FINDER.fileDownloadCountLimit()) {
+    private void checkDownloadLimit(FileDownloadDto todayDownload, int numberOfFiles) {
+        if (todayDownload.getCount() + numberOfFiles > PROPERTY_FINDER.fileDownloadCountLimit()) {
             throw new DownloadCountLimitExceededException();
         }
-    }
-
-    private String searchFileName(String query, String target) {
-        int startIdx = query.indexOf(target + "=");
-
-        if (startIdx == -1) {
-            throw new NotFoundQueryStringValueException();
-        }
-
-        return URLDecoder.decode(
-                query.substring(startIdx + target.length() + 1),
-                StandardCharsets.UTF_8);
     }
 
     private String searchSessionId(HttpHeaderField cookie) {
