2c21b2222037dca01739057bbe6a973454b9706c
eunhye park <lime1pm@naver.com>
2022-12-13 07:25:22 +0900

    FIX HttpRequestHandler

--- http/src/main/java/com/http/request/handler/HttpRequestHandler.java
+++ http/src/main/java/com/http/request/handler/HttpRequestHandler.java
@@ -1,12 +1,12 @@
 package com.http.request.handler;
 
-import com.http.HttpMessageStreams;
 import com.http.HttpRequestLengthLimiters;
 import com.http.RetryHttpRequestStream;
 import com.http.header.HttpHeaders;
 import com.http.request.HttpRequestPath;
 import com.http.request.HttpRequestQueryString;
+import com.http.response.HttpResponseStream;
 
 public interface HttpRequestHandler {
-    HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters);
+    HttpResponseStream handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters);
 }
