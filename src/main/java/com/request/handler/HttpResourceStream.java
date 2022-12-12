b4d64853980c41f44db803ed02d75069705872e6
eunhye park <lime1pm@naver.com>
2022-12-13 07:43:24 +0900

    FIX HttpResourceStream

--- src/main/java/com/request/handler/HttpResourceStream.java
+++ src/main/java/com/request/handler/HttpResourceStream.java
@@ -7,29 +7,27 @@ import com.http.request.HttpRequestPath;
 import com.http.request.HttpRequestQueryString;
 import com.http.request.handler.HttpRequestHandler;
 import com.http.response.HttpResponseStatus;
+import com.http.response.HttpResponseStream;
 
 public class HttpResourceStream implements HttpRequestHandler {
     private static final ResourceFinder RESOURCE_FINDER = new ResourceFinder();
     private static final HttpRequestPath DEFAULT_PATH = HttpRequestPath.of("/images");
 
     @Override
-    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
+    public HttpResponseStream handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
         if (httpRequestPath == null || httpHeaders == null || bodyStream == null) {
             throw new InputNullParameterException();
         }
 
         HttpRequestPath requestFilePath = DEFAULT_PATH.combine(httpRequestPath);
 
-        StringBuilder response = new StringBuilder();
-
-        response.append("HTTP/1.1 ")
-                .append(HttpResponseStatus.CODE_200.code()).append(" ")
-                .append(HttpResponseStatus.CODE_200.message()).append("\n")
-                .append("Content-Type: ").append(requestFilePath.contentType()).append("\n\n");
-
-        HttpMessageStreams responseMsg = HttpMessageStreams.of(response.toString());
+        HttpMessageStream responseHeaders = HttpMessageStream.of("Content-Type: "+requestFilePath.contentType()+";charset=UTF-8");
         HttpMessageStream fileStream = RESOURCE_FINDER.findGetRequestResource(requestFilePath);
 
-        return responseMsg.sequenceOf(fileStream);
+        return HttpResponseStream.from(
+                HttpResponseStatus.CODE_200,
+                responseHeaders,
+                fileStream
+        );
     }
 }
