f8a9feb2f59159bfb45f7a55031c935a135e1e6c
eunhye park <lime1pm@naver.com>
2022-12-13 07:42:30 +0900

    FIX HttpRequestPageStream

--- src/main/java/com/request/handler/HttpRequestPageStream.java
+++ src/main/java/com/request/handler/HttpRequestPageStream.java
@@ -1,59 +1,44 @@
 package com.request.handler;
 
-import com.http.*;
+import com.http.HttpMessageStream;
+import com.http.HttpRequestLengthLimiters;
+import com.http.RetryHttpRequestStream;
 import com.http.exception.InputNullParameterException;
 import com.http.header.HttpHeaders;
+import com.http.request.HttpRequestPath;
 import com.http.request.HttpRequestQueryString;
 import com.http.request.handler.HttpRequestHandler;
-import com.request.HttpRequestPagePath;
-import com.http.request.HttpRequestPath;
 import com.http.response.HttpResponseStatus;
+import com.http.response.HttpResponseStream;
+import com.request.HttpRequestPagePath;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 public class HttpRequestPageStream implements HttpRequestHandler {
 
     @Override
-    public HttpMessageStreams handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
+    public HttpResponseStream handle(HttpRequestPath httpRequestPath, HttpHeaders httpHeaders, RetryHttpRequestStream bodyStream, HttpRequestQueryString queryString, HttpRequestLengthLimiters requestLengthLimiters) {
         if (httpRequestPath == null || httpHeaders == null || bodyStream == null) {
             throw new InputNullParameterException();
         }
 
-        StringBuilder response = new StringBuilder();
-
-        response.append("HTTP/1.1 ")
-                .append(HttpResponseStatus.CODE_200.code()).append(" ")
-                .append(HttpResponseStatus.CODE_200.message()).append("\n")
-                .append("Content-Type: text/html;charset=UTF-8\n\n");
-
-        HttpMessageStreams responseMsg = HttpMessageStreams.of(response.toString());
+        HttpMessageStream responseHeaders = HttpMessageStream.of("Content-Type: text/html;charset=UTF-8");
 
         String pagePath = HttpRequestPagePath.of(httpRequestPath.getName()).path();
         InputStream pageIs = null;
         try {
             pageIs = new FileInputStream(pagePath);
         } catch (FileNotFoundException e) {
-            return createRedirectionResponse(HttpResponseStatus.CODE_500);
+            throw new RuntimeException(e);
         }
         HttpMessageStream pageHtml = HttpMessageStream.of(pageIs);
 
-        return responseMsg.sequenceOf(pageHtml);
-    }
-
-    private HttpMessageStreams createRedirectionResponse(HttpResponseStatus status) {
-        StringBuilder response = new StringBuilder();
-
-        response.append("HTTP/1.1 ")
-                .append(status.code()).append(" ")
-                .append(status.message()).append("\n");
-
-        InputStream responseIs = new ByteArrayInputStream(response.toString().getBytes(StandardCharsets.UTF_8));
-        StringStream responseStream = StringStream.of(responseIs);
-
-        return HttpMessageStreams.of(responseStream);
+        return HttpResponseStream.from(
+                HttpResponseStatus.CODE_200,
+                responseHeaders,
+                pageHtml
+        );
     }
 }
