32c2277c35e5a57dbd8e0e9da72bc74f47374958
eunhye park <lime1pm@naver.com>
2022-12-13 07:31:47 +0900

    FIX HttpMessageStream

--- http/src/main/java/com/http/HttpMessageStream.java
+++ http/src/main/java/com/http/HttpMessageStream.java
@@ -4,14 +4,16 @@ import com.http.exception.InputNullParameterException;
 import com.http.releaser.NoResourceCloser;
 import com.http.releaser.ResourceCloser;
 
+import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class HttpMessageStream implements Closeable {
-    private final StringStream stream;
+    private final ResourceStream stream;
     private final ResourceCloser closer;
 
-    private HttpMessageStream(StringStream stream, ResourceCloser closer) {
+    private HttpMessageStream(ResourceStream stream, ResourceCloser closer) {
         if (stream == null || closer == null) {
             throw new InputNullParameterException();
         }
@@ -20,29 +22,43 @@ public class HttpMessageStream implements Closeable {
         this.closer = closer;
     }
 
+    public static HttpMessageStream of(String str) {
+        InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+        return HttpMessageStream.of(is,new NoResourceCloser());
+    }
+
     public static HttpMessageStream of(InputStream inputStream) {
         return HttpMessageStream.of(inputStream,new NoResourceCloser());
     }
 
-    public static HttpMessageStream of(StringStream stringStream) {
-        return new HttpMessageStream(stringStream,new NoResourceCloser());
+    public static HttpMessageStream of(ResourceStream resourceStream) {
+        return new HttpMessageStream(resourceStream,new NoResourceCloser());
     }
 
     public static HttpMessageStream of(InputStream inputStream, ResourceCloser closer) {
-        StringStream stringStream = StringStream.of(inputStream);
-        return HttpMessageStream.of(stringStream,closer);
+        ResourceStream resourceStream = ResourceStream.of(inputStream);
+        return HttpMessageStream.of(resourceStream,closer);
     }
 
-    public static HttpMessageStream of(StringStream stringStream, ResourceCloser closer) {
-        return new HttpMessageStream(stringStream,closer);
+    public static HttpMessageStream of(ResourceStream resourceStream, ResourceCloser closer) {
+        return new HttpMessageStream(resourceStream,closer);
+    }
+
+    public static HttpMessageStream empty() {
+        InputStream is = new ByteArrayInputStream("".getBytes(StandardCharsets.UTF_8));
+        return HttpMessageStream.of(is);
     }
 
     public boolean hasMoreString() {
         return stream.hasMoreString();
     }
 
-    public String generate() {
-        return stream.generate();
+    public byte[] generateByte() {
+        return stream.generateByte();
+    }
+
+    public String generateString() {
+        return stream.generateString();
     }
 
     public String generateLine() {
