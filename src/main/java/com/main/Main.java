e0a58c01d200827cf3d641313c4033f7a67b1767
eunhye park <lime1pm@naver.com>
2022-12-15 04:46:04 +0900

    FIX Main

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -3,21 +3,17 @@ package com.main;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import com.db.dto.UserDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.http.Bootstrap;
 import com.http.PreProcessorComposite;
 import com.http.request.HttpRequestMethod;
 import com.http.request.HttpRequestPath;
+import com.http.request.handler.HttpRequestHandlers;
 import com.preprocessor.LoginPreProcessor;
 import com.request.handler.*;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     private static final LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
@@ -62,26 +58,26 @@ public class Main {
         PreProcessorComposite preProcessors = PreProcessorComposite.empty()
                 .sequenceOf(loginPreprocessor);
 
-        Bootstrap bootstrap = new Bootstrap(preProcessors);
+        HttpRequestHandlers handlers = HttpRequestHandlers.empty()
+            .register(HttpRequestPath.of("/oauth/google/signUp"), HttpRequestMethod.GET, new OauthCodeRequest())
+            .register(HttpRequestPath.of("/oauth/google/login"), HttpRequestMethod.GET, new OauthCodeRequest())
+            .register(HttpRequestPath.of("/oauth/google/signUp/callback"), HttpRequestMethod.GET, new OauthUserCreator())
+            .register(HttpRequestPath.of("/oauth/google/login/callback"), HttpRequestMethod.GET, new OauthUserFinder())
 
-        bootstrap.registerHandler(HttpRequestPath.of("/oauth/google/signUp"), HttpRequestMethod.GET, new OauthCodeRequest());
-        bootstrap.registerHandler(HttpRequestPath.of("/oauth/google/login"), HttpRequestMethod.GET, new OauthCodeRequest());
-        bootstrap.registerHandler(HttpRequestPath.of("/oauth/google/signUp/callback"), HttpRequestMethod.GET, new OauthUserCreator());
-        bootstrap.registerHandler(HttpRequestPath.of("/oauth/google/login/callback"), HttpRequestMethod.GET, new OauthUserFinder());
+            .register(HttpRequestPath.of("/signUp"), HttpRequestMethod.POST, new HttpRequestUserCreator())
+            .register(HttpRequestPath.of("/login"), HttpRequestMethod.POST, new HttpRequestUserFinder())
+            .register(HttpRequestPath.of("/logout"), HttpRequestMethod.POST, new HttpSessionCloser())
+            .register(HttpRequestPath.of("/upload"), HttpRequestMethod.POST, new HttpRequestFileUploader())
+            .register(HttpRequestPath.of("/download"), HttpRequestMethod.POST, new HttpRequestFileDownloader())
 
-        bootstrap.registerHandler(HttpRequestPath.of("/signUp"), HttpRequestMethod.POST, new HttpRequestUserCreator());
-        bootstrap.registerHandler(HttpRequestPath.of("/login"), HttpRequestMethod.POST, new HttpRequestUserFinder());
-        bootstrap.registerHandler(HttpRequestPath.of("/logout"), HttpRequestMethod.POST, new HttpSessionCloser());
-        bootstrap.registerHandler(HttpRequestPath.of("/upload"), HttpRequestMethod.POST, new HttpRequestFileUploader());
-        bootstrap.registerHandler(HttpRequestPath.of("/download"), HttpRequestMethod.POST, new HttpRequestFileDownloader());
+            .register(HttpRequestPath.of("/page/signUp"), HttpRequestMethod.GET, new HttpRequestPageStream())
+            .register(HttpRequestPath.of("/page/login"), HttpRequestMethod.GET, new HttpRequestPageStream())
+            .register(HttpRequestPath.of("/page/upload"), HttpRequestMethod.GET, new HttpRequestPageStream())
+            .register(HttpRequestPath.of("/page/download"), HttpRequestMethod.GET, new FileDownloadPageStream())
 
-        bootstrap.registerHandler(HttpRequestPath.of("/page/signUp"), HttpRequestMethod.GET, new HttpRequestPageStream());
-        bootstrap.registerHandler(HttpRequestPath.of("/page/login"), HttpRequestMethod.GET, new HttpRequestPageStream());
-        bootstrap.registerHandler(HttpRequestPath.of("/page/upload"), HttpRequestMethod.GET, new HttpRequestPageStream());
-        bootstrap.registerHandler(HttpRequestPath.of("/page/download"), HttpRequestMethod.GET, new FileDownloadPageStream());
+            .register(HttpRequestPath.ofResourcePath(), HttpRequestMethod.GET, new HttpResourceStream());
 
-        bootstrap.registerHandler(HttpRequestPath.ofResourcePath(), HttpRequestMethod.GET, new HttpResourceStream());
-
+        Bootstrap bootstrap = new Bootstrap(preProcessors,handlers);
         bootstrap.start();
     }
 
